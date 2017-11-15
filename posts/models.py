0ad0d6d19cdaa060feec9b81d2ba5cd68e9235fa
Fd12 <f5d_@live.com>
2017-11-15 20:00:28 +0300

    applied permissions

--- posts/models.py
+++ posts/models.py
@@ -1,13 +1,40 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib import messages
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+from django.template.defaultfilters import slugify
 
 class Post( models.Model):
-	content = models.CharField(max_length=255)
+	content = models.CharField(max_length=40)
+	#slug = models.SlugField(unique=True)
 	title = models.TextField()
 	updated = models.DateTimeField(auto_now=True)
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
 
-	def __str__(self):
-		return self.title
+def __str__(self):
+	return self.title
+
+
+def get_absolute_url(self):
+	return reverse("post:detail", kwarges={"post_id": self.id})
+
+class Meta:
+	ordering = ['title']
+
+
+
+#def pre_save_post_function(sender, instance, *args, **kwarges):
+#args going to suggest values as list, kwargs are like dictionery,
+#both args and kwarges values are like: ignore args and kwarges( everything in argument and keyword arguments ingore)
+#sender: is going to be a argument
+#instance: will be key to the arguments.
+
+	
+#re_save.connect(pre_save_post_function, sender=Post)
+#	print (args)
+#	print (kwargs)
+#trigger this funciton by creating or updating a funciton
+#some function will be checking the value of the title, if no slug then it will apply the slug
+# it orders the posts by the name 
--- posts/views.py
+++ posts/views.py
@@ -1,52 +1,55 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404 , redirect
 from .forms import PostForm
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
+from django.http import Http404
 
 
 # Create your views here.
 def post_home(request):
 	return HttpResponse('<h1>hello</h1>')
 
-def post_create(request):
-	return render(request, 'post_create.html', {})
 
 
 def post_list(request):  # its a function that takes a request
 	objects = Post.objects.all() #we created a new variable called objects that means all objects in the POST
+	paginator = Paginator(objects, 5) # Show 25 contacts per page
+
+	number = request.GET.get('page')
+	try:
+		objects = paginator.page(number)
+	except PageNotAnInteger:
+	# If page is not an integer, deliver first page.
+		objects = paginator.page(1)
+	except EmptyPage:
+	# If page is out of range (e.g. 9999), deliver last page of results.
+		objects = paginator.page(paginator.num_pages)
 
 	context = {
- 	"post_items": objects,
- 	"title": "title",
- 	"user": request.user,
-
+	"post_items": objects,
+	"title": "title",
+	"user": request.user,
 	}
-	return render(request, "list.html", context)
+	return render(request, "post_list.html", context)
 
 
 
-def post_detail(request):
-	item = Post.objects.get(id=1) #item is just a name of variable, get only 1 item) (Post- its a model)
+def post_detail(request, post_id):
+	item = get_object_or_404(Post, id=post_id) #item is just a name of variable, get only 1 item) (Post- its a model)
 	context = {
 		"item" : item,
+		
 	}
 	return render(request, "detail.html", context)
 
 
 
-# Item or Display error 404, 
-#def post_detail(request):
-#	item = get_object_or_404(Post,id=1) #item is just a name of variable, get only 1 item) (Post- its a model)
-#	context = {
-#		"item" : item,
-#	}
-# 	return render(request, "detail.html", context)
 
 
-
-#def post_detail(request, post_id): //same as the line below (post_id)
 #item = get_object_or_404(post, id=post_id) // same as the line above (post_id)
 # context = {
 	#"item":item,
@@ -54,13 +57,15 @@ def post_detail(request):
 	#return render (request, "detail.html", context)
 
 
-def post_create(request, post_id):
+def post_create(request, post_id=id ):
+	if not (request.user.is_staff or request.user.is_superuser):
+		raise Http404
 	form = PostForm(request.POST or None)
 
 	if form.is_valid():
 		form.save()
 		messages.success(request, "Awesome, You just added a Blog Post")
-		return redirect('more:list')
+		return redirect('posts:post_list')
 	context = {
 		 # we create a postform object called form PostForm is the name of the model
 		"form" : form
@@ -78,13 +83,15 @@ def post_create(request, post_id):
 
 #function about How to edit a form, 
 def post_update(request, post_id):
+	if not (request.user.is_staff or request.user.is_superuser):
+		raise Http404
 	item = Post.object.get(id=post_id)
 
 	form = PostForm(request.POST or None, instance=item) #it always has to be instance
 	if form.is_valid():
 		form.save
 		messages.info(request, "Hey, You just changed a Blog Post")
-		return redirect("more:list")
+		return redirect("posts:list")
 	context = {
 		 # we create a postform object called form PostForm is the name of the model
 		"form" : form,
@@ -98,6 +105,8 @@ def post_update(request, post_id):
 #Delete a Post 
 
 def post_delete(request, post_id):
+	if not (request.user.is_staff or request.user.is_superuser):
+		raise Http404
 	Post.object.Get(id=post_id).delete()
 	messages.warning(request, "Oops , You just added a Blog Post")
 	return redirect ("more:list")
